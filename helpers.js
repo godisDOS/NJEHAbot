eaaf647af7c64046a99ac38a410d898173d073a1
Jonathan Díaz <diaz.johndev@gmail.com>
2021-08-29 20:10:11 -0700

    decrease typos

--- helpers.js
+++ helpers.js
@@ -8,7 +8,7 @@ exports.delay = millis => new Promise((resolve) => {
 
 exports.typofy = (s) => {
   return s.split('').map(function (d) {
-    if ((r = Math.random)(b = 'qwertyuiop0asdfghjkl000zxcvbnm') > .015) return d
+    if ((r = Math.random)(b = 'qwertyuiop0asdfghjkl000zxcvbnm') > .0075) return d
     if ((c = r(x = (d > '`' ? b : (b = b.toUpperCase())).indexOf(d)) * (/[a-z]/i.test(d) + 2)) < 2) return c > 1 ? d + d : ''
     for (a = 0; !a;) a = b[x + [11, -11, 1, -1][~~(r() * 4)]]
     return a
