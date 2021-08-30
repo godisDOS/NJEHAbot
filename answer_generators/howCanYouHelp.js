87b81992a7bd93c425c730fb6803d653c33e6918
Jonathan Díaz <diaz.johndev@gmail.com>
2021-08-29 20:08:17 -0700

    add new answer narratives

--- answer_generators/howCanYouHelp.js
+++ answer_generators/howCanYouHelp.js
@@ -20,6 +20,8 @@ module.exports = () => {
 
 			if (rndInt(0, 10) >= 4) {
 				const attributes = sampleMany([
+					"loyal",
+					"dedicated",
 					"hard working",
 					"dedicated",
 					"adaptable",
@@ -64,6 +66,8 @@ module.exports = () => {
 			narrative = narrativeArr.join(" ");
 
 			narrative = narrative + " " + sample([
+				`I ${dictionary.believe} that we must take ${humanizeJoin(sampleMany(["ethnic", "govermental", "political", "economic", "social"], rndInt(1, 3)))} control away from the elites who have ${dictionary.usurped} power in our ${dictionary.homeland}.`,
+				`${humanizeJoin(sampleMany(["Ethnic", "Govermental", "Political", "Economic", "Social"], rndInt(1, 3)))} power must be taken away from the elites who have ${dictionary.usurped} power in our ${dictionary.homeland}.`,
 				`I am dedicated to preseving the ${white} race and people of today and tomorrow.`,
 				`My dedication is with the people of ${white} descent and I will use all skill to aid their existence.`,
 				`Given my background and skills I will give what I can to save ${white} people with all my might, ${sample(["for it is my duty", "as my duty calls", "until my last breath", "here and until the end", `for ${white} children`])}.`,
@@ -79,6 +83,69 @@ module.exports = () => {
 
 			return narrative;
 		})(),
+		(() => {
+			let narrativeArr = [];
+
+			// describe what exists
+			narrativeArr.push(
+				sample([
+					`In our ${dictionary.country} there are ${dictionary.efforts} to ${dictionary.eliminate} the ${white} race.`,
+					`For ${sample(["years", "decades", "centuries", "a long time"])} in our ${dictionary.country} there have been ${dictionary.efforts} to ${dictionary.eliminate} our people.`,
+					`There are ${dictionary.efforts} in our ${dictionary.homeland} to ${dictionary.eliminate} ${white} people that have gone on for very long.`,
+					`There is a world of information being hidden from the ${dictionary.silentMajority} about the ${dictionary.efforts} against ${white} people.`,
+					`As a ${white} descendant I cannot stand by the side lines as our ${dictionary.country} is ${dictionary.corrupted} against us.`,
+					`I cannot stand by the side lines as our ${dictionary.country} is ${dictionary.corrupted} against ${white} descendants.`,
+					`I cannot stand by the side lines as our ${dictionary.country} is ${dictionary.corrupted} by ${dictionary.elites}.`,
+				]) + " " +
+				sample([
+					`In ${dictionary.schools} students are taught to hate their own race and instead taught ideas of ${dictionary.theRed}.`,
+					`Within our ${dictionary.schools} we are taught to accept forced diversity and ${dictionary.theRed}.`,
+					`Increasingly the media is okay with teaching us to accept ${dictionary.theRed} as the norm.`,
+					`Our country is becoming ${dictionary.corrupted} by ideas of ${dictionary.theRed} which is furthered by ${dictionary.elites}.`,
+					`We are taught subservience as ${dictionary.foreigners} ${dictionary.invade} our ${dictionary.country}.`,
+					`People must accept forced ${dictionary.wokeness} or face ${dictionary.cancelCulture}.`,
+					`We are forced to behave through ${dictionary.wokeness} out of fear for ${dictionary.cancelCulture}.`,
+					`The masses will not listen to ${dictionary.facts} and instead follow the ${dictionary.media}.`
+				])
+			)
+			// we must
+			narrativeArr.push(
+				sample([
+					`Like our ${dictionary.foundingFathers} would have wanted we must stand up to our ${dictionary.expectations} and build a country for ${white} people once again.`,
+					`Just like the ${dictionary.foundingFathers} would have wished we must meet our ${dictionary.expectations} and rebuild our ${dictionary.homeland} which has been stolen.`,
+					`We must not passively accept a ${dictionary.corrupted} society for our children and rise up like our ${dictionary.foundingFathers} did for our ${dictionary.homeland}.`,
+					`It is a sin to passively wait for our ${dictionary.corrupted} society to be stolen from our children.`,
+					`We must act against the ${dictionary.wokeness} pushed by ${dictionary.elites} in order to preserve a future for the ${white} race.`,
+					`It is the duty of ${dictionary.patriots} to stand against the wave of ${dictionary.foreigners} in order to preserve a healthy future for ${white} children.`,
+					`For our children we must fulfil our ${dictionary.expectations} and fight against the ${dictionary.elites} for a better ${dictionary.country}.`,
+					`The ${white} man is not one to ${sample('whine', 'complain', 'fear')} as society is ${dictionary.corrupted}, but instead needs to act for their children and ${dictionary.homeland}.`,
+					`The ${sample('fact', 'simple fact', 'situation')} is that our ${dictionary.homeland} is being stolen and we must fight back to retake it.`,
+				]) + " " + sample([
+					`Blood must be payed in order to ensure the existence of the ${white} race and our descendants.`,
+					`We must dedicate ourselves to existence of the ${white} race and our children.`,
+					`There must be swift action to be taken to establish a new ${dictionary.homeland} where our children can thrive without fear of ${dictionary.foreigners} and ${dictionary.theRed}.`,
+					`We must act as ${white} men to a new ${dictionary.homeland} where our children can live without fear of ${dictionary.foreigners} and ${dictionary.theRed}.`,
+				])
+			)
+			// how i can help
+			narrativeArr.push(
+				...sampleMany([
+					`I am ${dictionary.dedicated} to give everything to ensure a future.`,
+					`We must give everything to ensure a better future.`,
+					`It is my duty to give everything to ensure our people from stopping the ${dictionary.efforts} to ${dictionary.eliminate} our people.`,
+					"We must establish the means to communicate with our people this message through media and controlled news.",
+					"We must take action to wake our people and call them into action.",
+					`We must take swift action against those that are destroyed our ${dictionary.homeland}.`,
+					`There must be efforts to ${humanizeJoin(["flyer", "campaign", "protest", "unify"])} in order to spread our message.`,
+					`We must engage our people and community through ${humanizeJoin(["flyering", "campaigning", "protesting"])} in order to spread our mission.`,
+				], 3)
+			)
+
+			narrativeArr = shuffle(narrativeArr);
+			narrative = narrativeArr.join(" ");
+
+			return narrative;
+		})()
 		// Duty of patriots
 		// Struggle for survival
 		// N2
--- dictionary.js
+++ dictionary.js
@@ -83,5 +83,147 @@ module.exports = {
 			"nation",
 			"state"
 		])
+	},
+	get usurped() {
+		return sample([
+			"usurped",
+			"taken",
+		])
+	},
+	get believe() {
+		return sample([
+			"believe",
+			"think"
+		])
+	},
+	get country() {
+		return sample([
+			"country",
+			this.homeland,
+			"territory",
+		])
+	},
+	get efforts() {
+		return sample([
+			"movements",
+			"efforts",
+			"conspiracies"
+		])
+	},
+	get eliminate() {
+		return sample([
+			"eliminate",
+			"genocide",
+			"remove",
+			"destroy",
+			"kill"
+		])
+	},
+	get schools() {
+		return sample([
+			"colleges",
+			"schools",
+			"college campuses",
+			"universities",
+		])
+	},
+	get theRed() {
+		return sample([
+			"marxism",
+			"communism",
+			"radicalism",
+			"liberalism",
+			"race realism",
+			"feminism",
+		])
+	},
+	get elites() {
+		return sample([
+			"elites",
+			"mass media",
+			"corporations",
+			"them",
+			"(((them)))",
+			"the ruling class",
+			`${this.theRed} media`,
+		])
+	},
+	get foreigners() {
+		return sample([
+			"foreigners",
+			"lessers",
+			"browns",
+			"immigrants"
+		])
+	},
+	get invade() {
+		return sample([
+			"invade",
+			"break into",
+			"sabotage",
+			"fill up",
+		])
+	},
+	get wokeness() {
+		return sample([
+			"wokeness",
+			"diversity",
+			"acceptance",
+			"tolerance",
+		])
+	},
+	get cancelCulture() {
+		return sample([
+			"cancel culture",
+			"cancelation",
+			"censorship",
+			"silencing",
+			"policial correctness"
+		])
+	},
+	get facts() {
+		return sample([
+			"facts",
+			"facts and logic",
+			"logic",
+			"simple facts",
+			"the facts",
+			"reason",
+		])
+	},
+	get media() {
+		return sample([
+			"media",
+			"mainstream media",
+		])
+	},
+	get corrupted() {
+		return sample([
+			"corrupted",
+			"corrupt",
+			"destroyed",
+			"degenerated",
+		])
+	},
+	get silentMajority() {
+		return sample([
+			"silent majority",
+			"people",
+			"masses",
+			"majority"
+		])
+	},
+	get foundingFathers() {
+		return sample([
+			"founding fathers",
+			"forefathers"
+		])
+	},
+	get dedicated() {
+		return sample([
+			"dedicated",
+			"focused",
+			"driven",
+		])
 	}
 }
